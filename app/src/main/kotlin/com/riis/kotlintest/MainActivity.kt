c7711f5a81d7704f0f8bb34e7de27d2fe7b0f575
Onorio Catenacci <ocatenacci@riis.com>
2015-03-27 14:02:30 -0400

    Initial version of Kotlin Android app--builds and works

---
+++ app/src/main/kotlin/com/riis/kotlintest/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.riis.kotlintest
+
+import android.support.v7.app.ActionBarActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+
+public class MainActivity : ActionBarActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
