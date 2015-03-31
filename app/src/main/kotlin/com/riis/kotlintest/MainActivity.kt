a2a9ec5d195544eef49352704d487c89d32c4309
Onorio Catenacci <ocatenacci@riis.com>
2015-03-31 18:27:52 -0400

    Getting the time displayed for two of the time zones

--- app/src/main/kotlin/com/riis/kotlintest/MainActivity.kt
+++ app/src/main/kotlin/com/riis/kotlintest/MainActivity.kt
@@ -14,6 +14,11 @@ public class MainActivity : ActionBarActivity() {
         setContentView(R.layout.activity_main)
         val t = TimeActivity()
         val times = t.GetTimesInMajorCities(array(getString(R.string.tz_name_New_York_City), getString(R.string.tz_name_Chicago), getString(R.string.tz_name_Denver), getString(R.string.tz_name_Los_Angeles)))
+        val nyc_tv:TextView? = findViewById(R.id.timeNYC) as? TextView
+        nyc_tv?.setText("Time in the " + times[0].cityName + " time zone is " + times[0].currentTimeInCity.toString())
+        val chi_tv:TextView = findViewById(R.id.timeChicago) as TextView
+        chi_tv.setText("Time in the " + times[1].cityName + " time zone is " + times[1].currentTimeInCity.toString())
+
 //        val initMsgTextView = getTextView("InitMsg")
     }
 }
