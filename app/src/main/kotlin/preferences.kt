120301355ef72c687656f04204072ec48339444d
Onorio Catenacci <catenacci@ieee.org>
2015-04-07 12:43:14 -0400

    WIP

---
+++ app/src/main/kotlin/preferences.kt
@@ -0,0 +1,15 @@
+
+/**
+ * Created by onorio_development on 4/7/15.
+ *
+ * Manage app preferences.
+ */
+import android.os.Bundle
+import android.preference.PreferenceActivity
+
+
+public class KotlinTestPreferences: PreferenceActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
