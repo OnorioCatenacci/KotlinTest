7ee0a15cb8186b7ad94bbd77b87de39458c21e4b
Onorio Catenacci <ocatenacci@riis.com>
2015-04-20 08:58:35 -0400

    SAVEPOINT

---
+++ app/src/main/kotlin/OnSwipeTouchListener.kt
@@ -0,0 +1,24 @@
+import android.content.Context
+import android.view.GestureDetector
+import android.view.View
+import android.view.MotionEvent
+import android.view.GestureDetector.SimpleOnGestureListener
+
+/**
+ * Created by ocatenacci on 4/17/2015.
+ */
+
+public class OnSwipeTouchListener(context:Context): View.OnTouchListener{
+
+    private val gestureDetector = GestureDetector(context, GestureListener())
+    public override fun onTouch(v:View, e:MotionEvent):Boolean{
+        return true
+    }
+
+    public fun onSwipeRight():Unit{
+    }
+
+    public fun onSwipeLeft():Unit{
+    }
+
+}
